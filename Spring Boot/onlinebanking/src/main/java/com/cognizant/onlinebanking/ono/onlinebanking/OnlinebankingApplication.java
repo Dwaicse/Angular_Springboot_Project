add3946d0150dd44695a8858288cfff6f0ab8126
Dwaicse <iamdwaipayan.bhattacharya@gmail.com>
2021-06-18 17:41:37 +0530

    Initial Commit

---
+++ Spring Boot/onlinebanking/src/main/java/com/cognizant/onlinebanking/ono/onlinebanking/OnlinebankingApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.onlinebanking.ono.onlinebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinebankingApplication.class, args);
+	}
+
+}
