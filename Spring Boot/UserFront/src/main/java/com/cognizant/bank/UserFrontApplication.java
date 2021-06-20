bd37c5ede53916efda26ca7831959f0dfed32a71
Arpan Chatterjee <83809814+arpan921@users.noreply.github.com>
2021-06-20 21:50:32 +0530

    UserFront created

---
+++ Spring Boot/UserFront/src/main/java/com/cognizant/bank/UserFrontApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserFrontApplication.class, args);
+	}
+
+}
