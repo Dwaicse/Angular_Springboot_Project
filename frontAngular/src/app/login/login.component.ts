ac8e561b43841b018c7b4c1b5dc61551edee7b98
Arpan Chatterjee <83809814+arpan921@users.noreply.github.com>
2021-06-21 14:42:14 +0530

    login basic created, connect to database

--- frontAngular/src/app/login/login.component.ts
+++ frontAngular/src/app/login/login.component.ts
@@ -7,18 +7,23 @@ import { AuthenticationServiceService } from '../authentication-service.service'
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  message=""
+  
   username:any;
   password:any;
+  invalidLogin:boolean=false;
+  errorMessage="Invalid Login"
   constructor(private authService:AuthenticationServiceService) { }
 
   ngOnInit(): void {
   }
 
   logIn(){
-    if(this.authService.verifyUser(this.username,this.password)){
-        this.message="login success!!"
+    if(!this.authService.verifyUser(this.username,this.password)){
+      this.invalidLogin=true;
+      // this.message="login success!!"
     }
+        
+    
   }
 
 }
