0dd279cc5d1325ee7fbad60c5649ff47de174371
Dwaicse <iamdwaipayan.bhattacharya@gmail.com>
2021-06-22 11:22:27 +0530

    Spring Boot First Commit

---
+++ Spring Boot/onlinebankingRestApi/src/test/java/com/cognizant/rest/onlinebankingservices/onlinebankingRestApi/OnlinebankingRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.rest.onlinebankingservices.onlinebankingRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinebankingRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
