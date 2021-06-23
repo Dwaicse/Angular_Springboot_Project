ae67728438a1b7113c7cd7d15cb4a27c0829f313
Arpan Chatterjee <83809814+arpan921@users.noreply.github.com>
2021-06-23 09:26:42 +0530

    signup component created, make html

--- frontAngular/src/app/app.module.ts
+++ frontAngular/src/app/app.module.ts
@@ -3,17 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FormsModule } from '@angular/forms';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     HeaderComponent,
-    DashboardComponent
+    DashboardComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
--- frontAngular/src/app/user.ts
+++ frontAngular/src/app/user.ts
@@ -1,5 +1,8 @@
 export class User {
     username: string =""
     password:string =""
+    firstname:String=""
+    lastName:string =""
+    
     
 }
