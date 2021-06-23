3a7761d995b7cd1f44996b403cf59560f46e1ed7
Dwaicse <iamdwaipayan.bhattacharya@gmail.com>
2021-06-24 00:23:41 +0530

    commit delete

--- Spring Boot/onlinebankingRestApi/src/main/java/com/cognizant/rest/onlinebankingservices/onlinebankingRestApi/Dao/AdminDao.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.rest.onlinebankingservices.onlinebankingRestApi.Dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cognizant.rest.onlinebankingservices.onlinebankingRestApi.controller.entities.Admin;
-
-public interface AdminDao extends JpaRepository<Admin, Long> {
-
-}
