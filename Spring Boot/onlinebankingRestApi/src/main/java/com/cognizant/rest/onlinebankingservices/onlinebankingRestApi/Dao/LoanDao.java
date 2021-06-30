06cfac60a0822d8511addbc94f4fa542b2e8f05a
Dwaicse <iamdwaipayan.bhattacharya@gmail.com>
2021-06-30 12:29:48 +0530

    All Refreshed

--- Spring Boot/onlinebankingRestApi/src/main/java/com/cognizant/rest/onlinebankingservices/onlinebankingRestApi/Dao/LoanDao.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.rest.onlinebankingservices.onlinebankingRestApi.Dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cognizant.rest.onlinebankingservices.onlinebankingRestApi.controller.entities.Loan;
-
-public interface LoanDao extends JpaRepository<Loan, Long> {
-
-}
